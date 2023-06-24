dbb7992534f89bdd04befe82c827184a0477c7be
Leonardo Figueiredo <65404185+lafigueiredo@users.noreply.github.com>
2023-06-24 11:57:13 -0300

    Update alter.php
    erase password

--- php/alter.php
+++ php/alter.php
@@ -4,7 +4,7 @@ $id = $_POST["id"];
 
 $host = "db-concessionaria.ch6oiaqhw5j6.us-east-1.rds.amazonaws.com";
 $username = "lafigueiredo";
-$password = "6tghL6y8dCVDaKzCrHvr";
+$password = "";
 $database = "db_concessionaria";
 
 // Conexão com o banco de dados
